98e7477298a129d3c610ee8ea3a87b53d947f2f7
meisterkoff <147273918+meisterkoff@users.noreply.github.com>
2025-10-25 22:29:23 +0200

    Refactor some code

--- app/src/main/java/com/victorkoffed/projektandroid/ui/screens/brew/BrewDetailScreen.kt
+++ app/src/main/java/com/victorkoffed/projektandroid/ui/screens/brew/BrewDetailScreen.kt
@@ -37,14 +37,12 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.DividerDefaults
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.FilterChip
 import androidx.compose.material3.FilterChipDefaults
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.IconButtonDefaults
--- app/src/main/java/com/victorkoffed/projektandroid/ui/screens/scale/ScaleConnectScreen.kt
+++ app/src/main/java/com/victorkoffed/projektandroid/ui/screens/scale/ScaleConnectScreen.kt
@@ -46,7 +46,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
